0ad95985cc29f88dbb7556faa3d1e560d481e49a
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2020-10-13 22:00:46 +0900

    PlayerInputAction設定

---
+++ survival_fps_project/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Vector2 moveVec2 {get; set;}
+    public Vector2 lookVec2 {get; set;}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

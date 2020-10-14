ef9350e226fe4d685c4484edffc871678a3b2b6a
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2020-10-14 21:40:34 +0900

    プレイヤーの基本的な移動(ジャンプなし)

---
+++ survival_fps_project/Assets/Scripts/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private PlayerMove player;
+
+    // InputActionsの登録と破棄
+    PlayerInputActions inputs;
+    private void Awake() => inputs = new PlayerInputActions();
+    private void OnDisable() => inputs.Disable();
+
+    // Inputの有効・無効化
+    void OnDestroy() => inputs.Disable();
+    void OnEnable() => inputs.Enable();
+
+    // Update is called once per frame
+    void Update()
+    {
+        player.moveVec2 = inputs.Player.Move.ReadValue<Vector2>();
+        player.lookVec2 = inputs.Player.Look.ReadValue<Vector2>();
+    }
+}
--- survival_fps_project/Assets/Scripts/PlayerMove.cs
+++ survival_fps_project/Assets/Scripts/PlayerMove.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMove : MonoBehaviour
 {
-    public Vector2 moveVec2 {get; set;}
-    public Vector2 lookVec2 {get; set;}
+    public Vector2 moveVec2 { get; set; }
+    public Vector2 lookVec2 { get; set; }
+
+    [Header("- player move setting and component-")]
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float targetVelocity = 10;
+    
+    [Header("- camera Aiming -")]
+    [SerializeField] private Transform camTransform;
+    [SerializeField] private float sensitivity = 10f;
+    private float yaw, pitch;
 
     // Start is called before the first frame update
     void Start()
@@ -13,9 +23,23 @@ public class PlayerMove : MonoBehaviour
         
     }
 
+    void FixedUpdate()
+    {
+        Vector3 v = new Vector3(moveVec2.x, 0f, moveVec2.y) * targetVelocity;
+        rb.AddRelativeForce(v * rb.mass * rb.drag / (1f - rb.drag * Time.fixedDeltaTime));
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        yaw += lookVec2.x * sensitivity * Time.deltaTime;
+        pitch -= lookVec2.y * sensitivity * Time.deltaTime;
+
+        pitch = Mathf.Clamp(pitch, -80, 60);
+
+        camTransform.localEulerAngles = new Vector3(pitch, 0, 0);
+        this.transform.eulerAngles = new Vector3(0, yaw, 0);
+
+        Debug.Log("move: "+ moveVec2 + " look: " + lookVec2);
     }
 }

0c9272c6f68d3ca86670a2bc8d9a9233e4bf1b40
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2020-10-24 16:03:14 +0900

    接地判定のRay位置を当たり判定に応じて動的変更

--- survival_fps_project/Assets/Scripts/PlayerMove.cs
+++ survival_fps_project/Assets/Scripts/PlayerMove.cs
@@ -10,15 +10,18 @@ public class PlayerMove : MonoBehaviour
     public bool jump { get; set; }
     public bool crouch { get; set; }
 
-    [Header("- player move setting and component-")]
+    [Header("- player walk settings and component-")]
     [SerializeField] private Rigidbody rb;
     [SerializeField] private CapsuleCollider playerCollider;
     [SerializeField] private float targetVelocity = 10;
     private float defaultDrag;
+
+    // player jump and ground check settings
     private float defaultPlayerHeight;
     [SerializeField] LayerMask rayLayer;
     public bool isGrounded;
-    [SerializeField] float groundRange = 1.1f;
+    [SerializeField] float groundDistance = 1.1f;
+    [SerializeField] float rayOriginHeightOffset = 0;
     [SerializeField] float jumpForce = 1.0f;
     
     [Header("- camera Aiming -")]
@@ -56,8 +59,15 @@ public class PlayerMove : MonoBehaviour
 
         camTransform.localEulerAngles = new Vector3(pitch, 0, 0);
         this.transform.eulerAngles = new Vector3(0, yaw, 0);
+        
+        float rayHeight = transform.position.y - (playerCollider.height / 2f) + playerCollider.center.y + rayOriginHeightOffset;
+        Ray groundRay = new Ray(
+             new Vector3(transform.position.x, rayHeight, transform.position.z),
+             Vector3.down
+        );
 
-        isGrounded = Physics.Raycast(transform.position, Vector3.down,groundRange,rayLayer);
+        isGrounded = Physics.Raycast(groundRay, groundDistance, rayLayer);
+        Debug.DrawRay(groundRay.origin, groundRay.direction * groundDistance, Color.red, 0.1f);
 
         if(crouch){
             playerCollider.height = defaultPlayerHeight / 2f;

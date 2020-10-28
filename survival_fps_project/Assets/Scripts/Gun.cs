41e786190bba4949bd45176175ce217b5676b602
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2020-10-28 21:36:44 +0900

    抽象クラス内の定義

--- survival_fps_project/Assets/Scripts/Gun.cs
+++ survival_fps_project/Assets/Scripts/Gun.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public abstract class Gun : MonoBehaviour
 {
+    // 発射時の初速
+    public float bulletSpeed = 100f;
+    
+    // 銃における弾丸の発射位置
+    public Transform muzzle;
+    
+    // 弾丸プレファブ
+    public GameObject bulletPrefab;
+
     public virtual void Shoot()
     {
         Debug.Log("GunShoot");
--- survival_fps_project/Assets/Scripts/TestGun.cs
+++ survival_fps_project/Assets/Scripts/TestGun.cs
@@ -7,5 +7,6 @@ public class TestGun : Gun
     public override void Shoot()
     {
         Debug.Log("Test Gun fire!");
+        
     }
 }

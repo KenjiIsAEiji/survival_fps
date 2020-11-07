f12bcd5aa87c0e75c09db11736169e6daf4e041a
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2020-11-07 21:31:35 +0900

    テスト用フルオート射撃銃をとりあえず実装

--- survival_fps_project/Assets/Scripts/TestFullAutoGun.cs
+++ survival_fps_project/Assets/Scripts/TestFullAutoGun.cs
@@ -10,9 +10,34 @@ public class TestFullAutoGun : Gun
     public override void Shoot(bool trigger)
     {
         if(trigger){
-
+            if(!triggerDownFlag){
+                triggerDownFlag = true;
+                StartCoroutine(triggerCycle());
+            }
         }else{
-
+            triggerDownFlag = false;
         }
+    }
+
+    IEnumerator triggerCycle()
+    {
+        while(true){
+            if(!triggerDownFlag) break;
+            fireBullet();
+            yield return new WaitForSeconds(fireRate);
+        }
+        yield break;
+    }
+
+    private void fireBullet()
+    {
+        GameObject bullet = Instantiate(
+            bulletPrefab,
+            muzzle.position,
+            muzzle.rotation
+        );
+
+        bullet.GetComponent<Rigidbody>().AddForce(muzzle.forward * bulletSpeed,ForceMode.Impulse);
+        Destroy(bullet,10f);
     }
 }
